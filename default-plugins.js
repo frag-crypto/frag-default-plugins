0dfc3430650652a6a115f300a2f02521a2853bc1
irontiga <irontiga@gmail.com>
2019-10-26 16:27:00 +1300

    update exported plugins format

--- default-plugins.js
+++ default-plugins.js
@@ -5,13 +5,15 @@ const plugins = [
     //     folder: path.join(__dirname, 'plugins/chat')
     // },
     {
-        folder: path.join(__dirname, 'plugins/core')
+        folder: path.join(__dirname, 'plugins/core'),
+        name: 'core'
     },
     // {
     //     folder: path.join(__dirname, 'plugins/rocket-chat')
     // },
     {
-        folder: path.join(__dirname, 'plugins/wallet')
+        folder: path.join(__dirname, 'plugins/wallet'),
+        name: 'wallet'
     }
 ]
 
